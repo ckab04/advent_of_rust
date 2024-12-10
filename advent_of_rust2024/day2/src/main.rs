7346f8b3a6f4720ee800437d9dd858def25e4e4f
ckab04 <ciceronkapenabm@gmail.com>
2024-12-10 10:59:29 +0100

    Day 2

---
+++ advent_of_rust2024/day2/src/main.rs
@@ -0,0 +1,10 @@
+fn main() {
+    let gift_message = String::from("Merry Christmas! Enjoy your gift!");
+        attach_message_to_present(&gift_message;
+
+        println!("{}", gift_message);
+}
+
+pub fn attach_message_to_present(message: &String) {
+    println!("The present now has this message: {}", message);
+}
