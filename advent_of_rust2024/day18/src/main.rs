e1907474ef6f4e21b9eac81bd33d7a06aa5c9838
ckab04 <ciceronkapenabm@gmail.com>
2025-01-10 22:49:07 +0100

    First attempt day 18

--- advent_of_rust2024/day18/src/main.rs
+++ advent_of_rust2024/day18/src/main.rs
@@ -1,3 +1,4 @@
+#[derive(Clone)]
 pub struct Sleigh {
     color: String,
     engine: String,
@@ -8,10 +9,54 @@ pub struct Sleigh {
 #[derive(Clone)]
 pub struct SleighBuilder {
     // Define the fields of SleighBuilder here
+     sleigh: Sleigh,
+}
+
+impl Default for Sleigh{
+   fn default() -> Self {
+       Self{
+           color: "red".to_string(),
+           engine: "reindeer-powerd".to_string(),
+           gift_capacity: 100,
+           magical_enhancements: false
+       }
+   }
 }
 
 impl SleighBuilder {
     // Your code here...
+   fn new() -> Self{
+       Self{
+            sleigh: Default::default(),
+           }
+   }
+
+   fn color(&mut self, color: &str) -> &mut Self{
+
+       self.sleigh.color = color.to_string();
+       self
+
+   }
+
+   fn engine(&mut self, engine: &str)  -> &mut Self{
+       self.sleigh.engine = engine.to_string();
+       self
+   }
+
+   fn gift_capacity(&mut self, gift_capacity: u32)  -> &mut Self{
+       self.sleigh.gift_capacity = gift_capacity;
+       self
+   }
+
+   fn magical_enhancements(&mut self)  -> &mut Self{
+       self.sleigh.magical_enhancements = true;
+       self
+   }
+
+   fn build(&self) -> Sleigh{
+       self.sleigh.clone()
+   }
+
 }
 
 // Don't Change this implementation
