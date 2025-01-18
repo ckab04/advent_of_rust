fc84689338bc9672ca81d6a64428e3bba532d763
ckab04 <ciceronkapenabm@gmail.com>
2025-01-18 23:06:31 +0100

    Trying a new approach

--- advent_of_rust2024/day19/src/main.rs
+++ advent_of_rust2024/day19/src/main.rs
@@ -4,50 +4,55 @@
 // - Flying
 
 
-#[derive(PartialEq)]
-pub enum StateSleigh{
-    Empty,
-    Ready,
-    Flying,
-}
+// #[derive(PartialEq)]
+// pub enum StateSleigh<State>{
+//     Empty,
+//     Ready,
+//     Flying,
+// }
 
 // 2. Finish the Seligh struct definition
-pub struct Sleigh{
-    state: StateSleigh,
-}
+// pub struct Sleigh{
+//     state: StateSleigh,
+// }
 
+
+pub struct Empty;
+pub struct Ready;
+pub struct Flying;
+
+fn main(){}
 
 // 3. Write the Sleigh Implementations for all states
 
-impl Sleigh{
-    fn new() -> Self{
-        Self{
-            state: StateSleigh::Empty
-            }
-    }
+// impl Sleigh{
+//     fn new() -> Self{
+//         Self{
+//             state: StateSleigh::Empty
+//             }
+//     }
 
-    fn load(&mut self){
-        if self.state == StateSleigh::Empty{
-            self.state = StateSleigh::Ready;
-        }
-    }
+//     fn load(&mut self){
+//         if self.state == StateSleigh::Empty{
+//             self.state = StateSleigh::Ready;
+//         }
+//     }
 
-    fn take_off(&mut self){
-        if self.state == StateSleigh::Ready{
-            self.state = StateSleigh::Flying;
-        }
-    }
+//     fn take_off(&mut self){
+//         if self.state == StateSleigh::Ready{
+//             self.state = StateSleigh::Flying;
+//         }
+//     }
 
-    fn unload(&mut self) {
-        if self.state == StateSleigh::Ready{
-            self.state = StateSleigh::Empty;
-        }
-    }
+//     fn unload(&mut self) {
+//         if self.state == StateSleigh::Ready{
+//             self.state = StateSleigh::Empty;
+//         }
+//     }
 
-    fn land(&mut self){
-        if self.state == StateSleigh::Flying{
-            self.state = StateSleigh::Ready;
-        }
-    }
-
-}
+//     fn land(&mut self){
+//         if self.state == StateSleigh::Flying{
+//             self.state = StateSleigh::Ready;
+//         }
+//     }
+//}
