fc8831e274b6d88ab194bb6f189d2bc693670eb1
ckab04 <ciceronkapenabm@gmail.com>
2025-02-06 22:36:52 +0100

    attempt to solve day21

--- advent_of_rust2024/day21/src/main.rs
+++ advent_of_rust2024/day21/src/main.rs
@@ -1,7 +1,15 @@
-use std::{fs::File, path::PathBuf};
+use std::{fs::{File, OpenOptions}, io::{Read, Write}, path::PathBuf};
 
 fn main() {
     println!("Hello, world!");
+}
+
+impl Drop for TempFile {
+    fn drop(&mut self) {
+        if let Err(e) = std::fs::remove_file(&self.file_path) {
+            eprintln!("Error removing temporary file: {}", e);
+        }
+    }
 }
 
 pub struct TempFile {
@@ -26,10 +34,16 @@ impl TempFile {
 
     pub fn write(&mut self, data: &[u8]) -> Result<(), std::io::Error> {
         // Your code here...
+        self.file.write_all(data)
     }
 
     pub fn read_to_string(&mut self) -> Result<String, std::io::Error> {
         // Your code here...
+        let mut file = OpenOptions::new().read(true).open(&self.file_path)?;
+        let mut file_content = String::new();
+        let _ = file.read_to_string(&mut file_content)?;
+        Ok(file_content)
+
     }
 
     pub fn path(&self) -> &PathBuf {
